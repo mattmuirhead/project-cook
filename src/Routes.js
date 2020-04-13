2417542f44934e59e78505d147bfe68eb6372c5f
Matt Muirhead <mat.rob.muirhead@googlemail.com>
2020-04-13 16:17:46 +0100

    Added pages

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import MyRecipes from './pages/MyRecipes'
+import AddRecipe from './pages/AddRecipe'
+import SearchRecipes from './pages/SearchRecipes'
 import AppNavigation from './components/molecules/AppNavigation'
 
 const Routes = () => (
@@ -9,6 +11,8 @@ const Routes = () => (
     <AppNavigation />
     <Switch>
       <Route path="/my-recipes" component={MyRecipes}/>
+      <Route path="/add-recipe" component={AddRecipe}/>
+      <Route path="/search-recipes" component={SearchRecipes}/>
       <Route path="/" component={Login}/>
     </Switch>
   </>
--- src/pages/AddRecipe/index.js
+++ src/pages/AddRecipe/index.js
@@ -3,17 +3,17 @@ import { useHistory } from 'react-router-dom'
 import { UserContext } from '../../providers/UserProvider'
 import { auth } from '../../Firebase/firebase'
 
-const MyRecipes = () => {
+const AddRecipe = () => {
   const user = useContext(UserContext)
   const history = useHistory()
 
   return (
     <>
       <div>Hi { user && user.firstName }</div>
-      <div>My Recipes</div>
+      <div>Add Recipe</div>
       <div onClick={() => auth.signOut().then(() => { history.push('/') })}>Logout</div>
     </>
   )
 }
 
-export default MyRecipes
+export default AddRecipe
